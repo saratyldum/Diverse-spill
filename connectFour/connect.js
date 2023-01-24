53f8f8fd5f5a34127317d3f4cdb48e7e0f6967de
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-01-24 11:29:36 +0100

    started connect four game

---
+++ connectFour/connect.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const squares = document.querySelectorAll('.grid div');
+	const result = document.querySelector('#result');
+	const displayCurrentPlayer = document.querySelector('#current-player');
+
+	let currentPlayer = 1;
+
+	
+})
